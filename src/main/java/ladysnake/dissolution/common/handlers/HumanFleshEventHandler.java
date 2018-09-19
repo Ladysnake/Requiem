484fe27af60d1642b9b1782162243a6cb2b2bf3a
Pyrofab <redstoneinfire@gmail.com>
2018-09-19 22:52:45 +0200

    Remove body purification in hardcore

--- src/main/java/ladysnake/dissolution/common/handlers/HumanFleshEventHandler.java
+++ src/main/java/ladysnake/dissolution/common/handlers/HumanFleshEventHandler.java
@@ -8,6 +8,7 @@ import ladysnake.dissolution.common.registries.SoulStates;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -19,7 +20,8 @@ public class HumanFleshEventHandler {
      */
     @SubscribeEvent
     public static void onLivingEntityUseItem(LivingEntityUseItemEvent.Finish event) {
-        if (event.getEntity().world.isRemote) {
+        World world = event.getEntity().world;
+        if (world.isRemote || world.getMinecraftServer().isHardcore()) {
             return;
         }
         if (OreDictHelper.doesItemMatch(event.getItem(), OreDictHelper.HUMAN_FLESH_RAW) || event.getItem().getItem() instanceof ItemHumanFlesh || event.getItem().getItem() == Items.GOLDEN_APPLE) {
