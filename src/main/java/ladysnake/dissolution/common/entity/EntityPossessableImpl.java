1623901b3563915eba62c09427941fa4d7deace8
Pyrofab <redstoneinfire@gmail.com>
2018-12-27 19:36:38 +0100

    Synchronize potion effects between player and possessed

--- src/main/java/ladysnake/dissolution/common/entity/EntityPossessableImpl.java
+++ src/main/java/ladysnake/dissolution/common/entity/EntityPossessableImpl.java
@@ -1,6 +1,8 @@
 package ladysnake.dissolution.common.entity;
 
 import com.google.common.base.Optional;
+import ladylib.reflection.TypedReflection;
+import ladylib.reflection.typed.TypedMethod0;
 import ladysnake.dissolution.api.corporeality.IIncorporealHandler;
 import ladysnake.dissolution.api.corporeality.IPossessable;
 import ladysnake.dissolution.common.Dissolution;
@@ -9,6 +11,7 @@ import ladysnake.dissolution.common.capabilities.CapabilityIncorporealHandler;
 import ladysnake.dissolution.common.entity.ai.EntityAIInert;
 import ladysnake.dissolution.common.entity.ai.attribute.AttributeHelper;
 import ladysnake.dissolution.common.entity.ai.attribute.CooldownStrengthAttribute;
+import ladysnake.dissolution.common.util.CollectionUtil;
 import ladysnake.dissolution.common.util.DelayedTaskRunner;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
@@ -39,6 +42,9 @@ import org.lwjgl.util.vector.Vector2f;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -50,6 +56,7 @@ import java.util.UUID;
 public class EntityPossessableImpl extends EntityMob implements IPossessable {
     private static final DataParameter<Optional<UUID>> POSSESSING_ENTITY_ID =
             EntityDataManager.createKey(EntityPossessableImpl.class, DataSerializers.OPTIONAL_UNIQUE_ID);
+    private static final TypedMethod0<PotionEffect, Integer> deincrementDuration = TypedReflection.findMethod(PotionEffect.class, "func_76454_e", int.class);
 
     private EntityAIInert aiDontDoShit = new EntityAIInert(false);
 
@@ -58,6 +65,7 @@ public class EntityPossessableImpl extends EntityMob implements IPossessable {
     private double prevMotionX;
     private double prevMotionY;
     private double prevMotionZ;
+    private Set<PotionEffect> prevPotionEffects = new HashSet<>();
 
     public EntityPossessableImpl(World worldIn) {
         super(worldIn);
@@ -143,27 +151,37 @@ public class EntityPossessableImpl extends EntityMob implements IPossessable {
         EntityPlayer possessing = getPossessingEntity();
         if (possessing != null) {
             this.setPosition(possessing.posX, possessing.posY, possessing.posZ);
-//            possessing.setPosition(this.posX, this.posY, this.posZ);
-            //                for (PotionEffect potionEffect : ((EntityPlayer) possessing).getActivePotionMap().values())
-//                    this.addPotionEffect(new PotionEffect(potionEffect));
-            possessing.clearActivePotions();
-            for (PotionEffect potionEffect : this.getActivePotionMap().values()) {
-                possessing.addPotionEffect(new PotionEffect(potionEffect));
-            }
         }
     }
 
     @Override
     public void onUpdate() {
         if (!world.isRemote) {
-            if (this.prevMotionX != this.motionX || this.prevMotionY != this.motionY || this.prevMotionZ != this.motionZ) {
-                if (this.isBeingPossessed()) {
-                    EntityPlayer possessing = this.getPossessingEntity();
+            if (this.isBeingPossessed()) {
+                EntityPlayer possessing = Objects.requireNonNull(this.getPossessingEntity());
+                if (this.prevMotionX != this.motionX || this.prevMotionY != this.motionY || this.prevMotionZ != this.motionZ) {
                     possessing.motionX += this.motionX - this.prevMotionX;
                     possessing.motionY += this.motionY - this.prevMotionY;
                     possessing.motionZ += this.motionZ - this.prevMotionZ;
                     possessing.velocityChanged = true;
                 }
+                // Tick down potion effects from previous tick
+                prevPotionEffects.removeIf(p -> deincrementDuration.invoke(p) <= 0);
+                // Merge changes in potion effects from both entities
+                prevPotionEffects = CollectionUtil.mergeHistories(
+                        new HashSet<>(this.getActivePotionEffects()),
+                        new HashSet<>(possessing.getActivePotionEffects()),
+                        prevPotionEffects
+                );
+                // Resynchronize both entities
+                this.clearActivePotions();
+                possessing.clearActivePotions();
+                for (PotionEffect e : prevPotionEffects) {
+                    this.addPotionEffect(new PotionEffect(e));
+                    possessing.addPotionEffect(new PotionEffect(e));
+                }
+            } else {
+                prevPotionEffects.clear();
             }
         }
         super.onUpdate();
