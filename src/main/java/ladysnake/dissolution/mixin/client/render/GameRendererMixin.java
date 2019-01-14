1aecd77eaa075df76957f9738bae7a6e99babff6
Pyrofab <redstoneinfire@gmail.com>
2019-01-14 23:34:09 +0100

    Prevent possessed entities from hitting themselves

---
+++ src/main/java/ladysnake/dissolution/mixin/client/render/GameRendererMixin.java
@@ -0,0 +1,40 @@
+package ladysnake.dissolution.mixin.client.render;
+
+import ladysnake.dissolution.api.possession.Possessor;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
+
+import static org.spongepowered.asm.mixin.injection.At.Shift.BY;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(
+            method = "method_3190",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/world/ClientWorld;" +
+                            "getEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/BoundingBox;Ljava/util/function/Predicate;)" +
+                            "Ljava/util/List;",
+                    shift = BY,
+                    by = 2
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    public void unselectPossessedEntity(float tickDelta, CallbackInfo info,
+                                        Entity camera, double double_1, Vec3d vec3d_1, boolean boolean_1, int int_1, double double_2, Vec3d vec3d_2, Vec3d vec3d_3, Vec3d vec3d_4, float float_2,
+                                        List<Entity> entities) {
+        if (camera instanceof Possessor && ((Possessor) camera).isPossessing()) {
+            // Possessable are still entities
+            //noinspection SuspiciousMethodCalls
+            entities.remove(((Possessor) camera).getPossessedEntity());
+        }
+    }
+}
