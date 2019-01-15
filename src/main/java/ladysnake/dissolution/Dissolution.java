bf7fafb1c6ecf80064f2f6d0a87aa0a271cfcba1
Pyrofab <redstoneinfire@gmail.com>
2019-01-15 22:22:38 +0100

    BEGONE SPACES

--- src/main/java/ladysnake/dissolution/Dissolution.java
+++ src/main/java/ladysnake/dissolution/Dissolution.java
@@ -11,19 +11,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Dissolution implements ModInitializer {
-	public static final String MOD_ID = "dissolution";
-	public static final Logger LOGGER = LogManager.getLogger("Dissolution");
+    public static final String MOD_ID = "dissolution";
+    public static final Logger LOGGER = LogManager.getLogger("Dissolution");
 
-	public static Identifier id(String path) {
-	    return new Identifier(MOD_ID, path);
+    public static Identifier id(String path) {
+        return new Identifier(MOD_ID, path);
     }
 
-	@Override
-	public void onInitialize() {
-		DissolutionBlocks.init();
-		DissolutionEntities.init();
-		DissolutionItems.init();
-		DefaultRemnantHandler.init();
-		Possession.init();
-	}
+    @Override
+    public void onInitialize() {
+        DissolutionBlocks.init();
+        DissolutionEntities.init();
+        DissolutionItems.init();
+        DefaultRemnantHandler.init();
+        Possession.init();
+    }
 }
