0e2a35c485b78f2c1c7e7ccee3097d08ec27b227
Pyrofab <redstoneinfire@gmail.com>
2019-01-15 09:53:38 +0100

    Render mob arm in first person

--- src/main/java/ladysnake/dissolution/mixin/client/entity/PlayerEntityRendererMixin.java
+++ src/main/java/ladysnake/dissolution/mixin/client/entity/PlayerEntityRendererMixin.java
@@ -5,10 +5,21 @@ import ladysnake.dissolution.api.DissolutionPlayer;
 import ladysnake.dissolution.api.possession.Possessor;
 import ladysnake.dissolution.api.remnant.RemnantHandler;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.Cuboid;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.sortme.OptionMainHand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,7 +27,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static org.spongepowered.asm.mixin.injection.At.Shift.AFTER;
 
 @Mixin(PlayerEntityRenderer.class)
-public abstract class PlayerEntityRendererMixin {
+public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+
+    public PlayerEntityRendererMixin(EntityRenderDispatcher entityRenderDispatcher_1, PlayerEntityModel<AbstractClientPlayerEntity> entityModel_1, float float_1) {
+        super(entityRenderDispatcher_1, entityModel_1, float_1);
+    }
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     public void cancelRender(AbstractClientPlayerEntity renderedPlayer, double x, double y, double z, float yaw, float tickDelta, CallbackInfo info) {
@@ -52,5 +67,41 @@ public abstract class PlayerEntityRendererMixin {
     )
     public void postRender(AbstractClientPlayerEntity renderedPlayer, double x, double y, double z, float yaw, float tickDelta, CallbackInfo info) {
         GlStateManager.color4f(1f, 1f, 1f, 1f);
+    }
+
+    @Shadow
+    protected abstract void setModelPose(AbstractClientPlayerEntity player);
+
+    @SuppressWarnings("InvalidMemberReference")
+    @Inject(method = {"method_4220", "method_4221"}, at = @At("HEAD"), cancellable = true)
+    public void renderPossessedHand(AbstractClientPlayerEntity renderedPlayer, CallbackInfo info) {
+        if (((Possessor)renderedPlayer).isPossessing()) {
+            LivingEntity possessed = (LivingEntity)((Possessor) renderedPlayer).getPossessedEntity();
+            if (possessed != null) {
+                EntityRenderer renderer = MinecraftClient.getInstance().getEntityRenderManager().getRenderer(possessed);
+                // If the mob has an arm, render it instead of the player's
+                if (renderer instanceof FeatureRendererContext) {
+                    Model model = ((LivingEntityRenderer) renderer).getModel();
+                    if (model instanceof BipedEntityModel) {
+                        boolean rightArm = renderedPlayer.getMainHand() == OptionMainHand.RIGHT;
+                        GlStateManager.color3f(1.0F, 1.0F, 1.0F);
+                        BipedEntityModel playerEntityModel_1 = (BipedEntityModel) model;
+                        this.setModelPose(renderedPlayer);
+                        GlStateManager.enableBlend();
+                        playerEntityModel_1.swingProgress = 0.0F;
+                        playerEntityModel_1.isSneaking = false;
+                        playerEntityModel_1.field_3396 = 0.0F;
+                        //noinspection unchecked
+                        playerEntityModel_1.method_17087(possessed, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
+                        Cuboid arm = rightArm ? playerEntityModel_1.armRight : playerEntityModel_1.armLeft;
+                        arm.pitch = 0.0F;
+                        arm.render(0.0625F);
+                        GlStateManager.disableBlend();
+                    }
+                }
+                // prevent rendering the player's arm regardless
+                info.cancel();
+            }
+        }
     }
 }
