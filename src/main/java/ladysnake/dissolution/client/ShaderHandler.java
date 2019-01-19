06686a7d7c55425075349e11fc5cc69b3b29e334
Pyrofab <redstoneinfire@gmail.com>
2019-01-19 17:47:56 +0100

    Fish eye shader

--- src/main/java/ladysnake/dissolution/client/ShaderHandler.java
+++ src/main/java/ladysnake/dissolution/client/ShaderHandler.java
@@ -10,16 +10,22 @@ import net.minecraft.util.Identifier;
 
 public final class ShaderHandler implements FloatConsumer {
     public static final Identifier SPECTRE_SHADER_ID = Dissolution.id("shaders/post/spectre.json");
+    public static final Identifier FISH_EYE_SHADER_ID = Dissolution.id("shaders/post/fish_eye.json");
 
     public static final ShaderHandler INSTANCE = new ShaderHandler();
 
     private final MinecraftClient mc = MinecraftClient.getInstance();
     private final ManagedShaderEffect spectreShader = ShaderEffectManager.manage(SPECTRE_SHADER_ID);
+    private final ManagedShaderEffect fishEyeShader = ShaderEffectManager.manage(FISH_EYE_SHADER_ID);
+    private boolean renderFishEye = false;
 
     @Override
     public void accept(float tickDelta) {
         if (RemnantHandler.get(mc.player).filter(RemnantHandler::isIncorporeal).isPresent()) {
             spectreShader.render(tickDelta);
+        }
+        if (renderFishEye) {
+            fishEyeShader.render(tickDelta);
         }
     }
 }
