f381656d1ef0d5a04f5664b28d54ac4a4908facf
Pyrofab <redstoneinfire@gmail.com>
2019-02-22 16:20:53 +0100

    Prevent food bar from rendering during possession

--- src/main/java/ladysnake/dissolution/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/ladysnake/dissolution/mixin/client/gui/hud/InGameHudMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
@@ -15,12 +16,19 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
 
     @Shadow @Final private MinecraftClient client;
+
+    @Shadow @Nullable protected abstract PlayerEntity getCameraPlayer();
+
+    @Shadow protected abstract int method_1744(LivingEntity livingEntity_1);
 
     @Inject(
             method = "renderStatusBars",
@@ -31,6 +39,22 @@ public abstract class InGameHudMixin extends DrawableHelper {
             // Make everything that follows *invisible*
             GlStateManager.color4f(1, 1, 1, 0);
         }
+    }
+
+    @Redirect(
+            method = "renderStatusBars",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;method_1744(Lnet/minecraft/entity/LivingEntity;)I"
+            )
+    )
+    private int preventFoodRender(InGameHud inGameHud, LivingEntity livingEntity_1) {
+        int actual = this.method_1744(livingEntity_1);
+        DissolutionPlayer cameraPlayer = (DissolutionPlayer) this.getCameraPlayer();
+        if (actual == 0 && cameraPlayer != null && cameraPlayer.getPossessionComponent().isPossessing()) {
+            return -1;
+        }
+        return actual;
     }
 
     @ModifyVariable(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/SystemUtil;getMeasuringTimeMs()J"), ordinal = 0)
