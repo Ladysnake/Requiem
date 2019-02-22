0c5cb18d7304ec4089b0d1cd18e8d7a1ae89967a
Pyrofab <redstoneinfire@gmail.com>
2019-02-22 16:43:49 +0100

    Delegate player breath to their possessed entity

--- src/main/java/ladysnake/dissolution/mixin/entity/EntityMixin.java
+++ src/main/java/ladysnake/dissolution/mixin/entity/EntityMixin.java
@@ -1,9 +1,38 @@
 package ladysnake.dissolution.mixin.entity;
 
+import ladysnake.dissolution.api.v1.DissolutionPlayer;
+import ladysnake.dissolution.api.v1.possession.PossessionComponent;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
+    @Inject(method = "getMaxBreath", at = @At("HEAD"), cancellable = true)
+    private void delegateMaxBreath(CallbackInfoReturnable<Integer> cir) {
+        if (this instanceof DissolutionPlayer) {
+            PossessionComponent possessionComponent = ((DissolutionPlayer) this).getPossessionComponent();
+            if (possessionComponent != null) {
+                Entity possessedEntity = (Entity) possessionComponent.getPossessedEntity();
+                if (possessedEntity != null) {
+                    cir.setReturnValue(possessedEntity.getMaxBreath());
+                }
+            }
+        }
+    }
 
+    @Inject(method = "getBreath", at = @At("HEAD"), cancellable = true)
+    private void delegateBreath(CallbackInfoReturnable<Integer> cir) {
+        if (this instanceof DissolutionPlayer) {
+            PossessionComponent possessionComponent = ((DissolutionPlayer) this).getPossessionComponent();
+            if (possessionComponent != null) {
+                Entity possessedEntity = (Entity) ((DissolutionPlayer) this).getPossessionComponent().getPossessedEntity();
+                if (possessedEntity != null) {
+                    cir.setReturnValue(possessedEntity.getBreath());
+                }
+            }
+        }
+    }
 }
