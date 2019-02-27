232db72ca43068cadc3bc1ff1ac70ca79e651340
Pyrofab <redstoneinfire@gmail.com>
2019-02-27 16:39:35 +0100

    Skip death screen when remnant
    @doctor4t @doctor4t @doctor4t

--- src/main/java/ladysnake/dissolution/mixin/client/MinecraftClientMixin.java
+++ src/main/java/ladysnake/dissolution/mixin/client/MinecraftClientMixin.java
@@ -1,9 +1,13 @@
 package ladysnake.dissolution.mixin.client;
 
 import ladysnake.dissolution.api.v1.DissolutionPlayer;
+import ladysnake.dissolution.api.v1.remnant.RemnantState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Screen;
+import net.minecraft.client.gui.ingame.DeathScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -41,6 +45,20 @@ public abstract class MinecraftClientMixin {
         if (!this.interactionManager.isBreakingBlock() && !this.player.method_3144()) {
             if (((DissolutionPlayer) player).getPossessionComponent().isPossessing() && player.getMainHandStack().isEmpty()) {
                 sendToServer(createRightClickMessage());
+            }
+        }
+    }
+
+    @Inject(
+            method = "openScreen",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;currentScreen:Lnet/minecraft/client/gui/Screen;", ordinal = 0, opcode = Opcodes.PUTFIELD),
+            cancellable = true
+    )
+    private void skipDeathScreen(Screen screen, CallbackInfo ci) {
+        if (screen instanceof DeathScreen) {
+            if (RemnantState.getIfRemnant(this.player).isPresent()) {
+                this.player.requestRespawn();
+                ci.cancel();
             }
         }
     }
