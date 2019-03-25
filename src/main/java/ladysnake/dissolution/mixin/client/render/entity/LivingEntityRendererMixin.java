9d3c9a1b79fdfda157bb57655a83f060387ac567
Pyrofab <redstoneinfire@gmail.com>
2019-03-25 22:48:20 +0100

    Let possessed undeads control undead horses

--- src/main/java/ladysnake/dissolution/mixin/client/render/entity/LivingEntityRendererMixin.java
+++ src/main/java/ladysnake/dissolution/mixin/client/render/entity/LivingEntityRendererMixin.java
@@ -14,6 +14,7 @@ import javax.annotation.Nullable;
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin {
 
+    @SuppressWarnings("UnresolvedMixinReference")   // Minecraft dev plugin is an idiot sandwich
     @Nullable
     @Redirect(method = "method_4054", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getRiddenEntity()Lnet/minecraft/entity/Entity;"))
     private Entity getPossessorRiddenEntity(LivingEntity entity) {
@@ -24,6 +25,7 @@ public abstract class LivingEntityRendererMixin {
         return entity.getRiddenEntity();
     }
 
+    @SuppressWarnings("UnresolvedMixinReference")   // Minecraft dev plugin is an idiot sandwich
     @Redirect(method = "method_4054", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;hasVehicle()Z"))
     private boolean doesPossessorHaveVehicle(LivingEntity entity) {
         PlayerEntity possessor = ((Possessable) entity).getPossessor();
