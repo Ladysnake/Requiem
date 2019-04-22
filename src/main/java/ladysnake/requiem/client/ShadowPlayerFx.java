0d3620e0377091cc5af6c9af314d574e33d89e3d
Pyrofab <redstoneinfire@gmail.com>
2019-04-22 20:11:03 +0200

    Implement desaturation when approaching an ethereal player

--- src/main/java/ladysnake/requiem/client/ShadowPlayerFx.java
+++ src/main/java/ladysnake/requiem/client/ShadowPlayerFx.java
@@ -3,15 +3,18 @@ package ladysnake.requiem.client;
 import com.mojang.blaze3d.platform.GLX;
 import com.mojang.blaze3d.platform.GlStateManager;
 import ladysnake.requiem.Requiem;
+import ladysnake.requiem.api.v1.RequiemPlayer;
 import ladysnake.satin.api.event.EntitiesPreRenderCallback;
 import ladysnake.satin.api.event.ShaderEffectRenderCallback;
 import ladysnake.satin.api.experimental.ReadableDepthFramebuffer;
 import ladysnake.satin.api.managed.ManagedShaderEffect;
 import ladysnake.satin.api.managed.ShaderEffectManager;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gl.GlFramebuffer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VisibleRegion;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.GL11;
 
@@ -23,20 +26,37 @@ import java.util.Objects;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
 
 public final class ShadowPlayerFx implements EntitiesPreRenderCallback, ShaderEffectRenderCallback {
-    public static final Identifier SHADOW_PLAYER_ID = Requiem.id("shaders/post/shadow_player.json");
+    public static final Identifier SHADOW_PLAYER_SHADER_ID = Requiem.id("shaders/post/shadow_player.json");
+    public static final Identifier DESATURATE_SHADER_ID = new Identifier("minecraft:desaturate.json");
 
     public static final ShadowPlayerFx INSTANCE = new ShadowPlayerFx();
+    public static final int ETHEREAL_DESATURATE_RANGE = 16;
+    public static final int ETHEREAL_DESATURATE_RANGE_SQ = ETHEREAL_DESATURATE_RANGE * ETHEREAL_DESATURATE_RANGE;
 
     private final MinecraftClient client = MinecraftClient.getInstance();
-    public final ManagedShaderEffect shadowPlayerEffect = ShaderEffectManager.getInstance().manage(SHADOW_PLAYER_ID, this::assignDepthTexture);
+    public final ManagedShaderEffect shadowPlayerEffect = ShaderEffectManager.getInstance().manage(SHADOW_PLAYER_SHADER_ID, this::assignDepthTexture);
+    private final ManagedShaderEffect desaturateEffect = ShaderEffectManager.getInstance().manage(DESATURATE_SHADER_ID);
 
     @Nullable
     private GlFramebuffer playersFramebuffer;
     private boolean renderedSoulPlayers;
+    private float distanceToEthereal = Float.NaN;
+    private boolean nearEthereal;
 
     void registerCallbacks() {
         EntitiesPreRenderCallback.EVENT.register(this);
         ShaderEffectRenderCallback.EVENT.register(this);
+        ClientTickCallback.EVENT.register(this::update);
+    }
+
+    private void update(MinecraftClient client) {
+        if (client.player != null) {
+            PlayerEntity closestEtherealPlayer = client.world.getClosestPlayer(client.player.x, client.player.y, client.player.z, ETHEREAL_DESATURATE_RANGE, p -> ((RequiemPlayer)p).getRemnantState().isIncorporeal());
+            if (closestEtherealPlayer != null) {
+                float distanceSqToEthereal = (float) client.player.squaredDistanceTo(closestEtherealPlayer.x, closestEtherealPlayer.y, closestEtherealPlayer.z);
+                this.desaturateEffect.setUniformValue("Saturation", 0.8f * (distanceSqToEthereal / ETHEREAL_DESATURATE_RANGE_SQ));
+            }
+        }
     }
 
     private void assignDepthTexture(ManagedShaderEffect shader) {
@@ -78,6 +98,9 @@ public final class ShadowPlayerFx implements EntitiesPreRenderCallback, ShaderEf
     public void renderShaderEffects(float tickDelta) {
         if (this.renderedSoulPlayers) {
             shadowPlayerEffect.render(tickDelta);
+        }
+        if (this.nearEthereal) {
+            this.desaturateEffect.render(tickDelta);
         }
     }
 
