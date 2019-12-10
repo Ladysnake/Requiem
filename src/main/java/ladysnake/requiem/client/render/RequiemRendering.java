a55dcf54cf05242e86a06fdbaf5e27dee800ece0
Pyrofab <redstoneinfire@gmail.com>
2019-12-10 01:51:11 +0100

    Bump version

--- src/main/java/ladysnake/requiem/client/render/RequiemRendering.java
+++
@@ -1,9 +0,0 @@
-package ladysnake.requiem.client.render;
-
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.VertexConsumerProvider;
-
-public class RequiemRendering {
-    private static final VertexConsumerProvider.Immediate entityVertexConsumers = VertexConsumerProvider.immediate(new BufferBuilder(255));
-
-}
