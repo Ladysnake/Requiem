8da884547191e1453f4f781b27a522352022d8ff
Pyrofab <redstoneinfire@gmail.com>
2020-01-01 21:11:42 +0100

    Fix custom gamerules not loading on servers

--- src/main/java/ladysnake/requiem/Requiem.java
+++ src/main/java/ladysnake/requiem/Requiem.java
@@ -25,7 +25,6 @@ import ladysnake.requiem.common.RequiemRegistries;
 import ladysnake.requiem.common.advancement.criterion.RequiemCriteria;
 import ladysnake.requiem.common.block.RequiemBlocks;
 import ladysnake.requiem.common.command.RequiemCommand;
-import ladysnake.requiem.common.gamerule.RequiemGamerules;
 import ladysnake.requiem.common.impl.ApiInitializer;
 import ladysnake.requiem.common.impl.movement.MovementAltererManager;
 import ladysnake.requiem.common.impl.remnant.dialogue.ReloadableDialogueRegistry;
@@ -69,7 +68,6 @@ public class Requiem implements ModInitializer {
         RequiemComponents.initComponents();
         SyncServerResourcesCallback.EVENT.register(player -> RequiemNetworking.sendTo(player, RequiemNetworking.createDataSyncMessage(SubDataManagerHelper.getServerHelper())));
         ApiInitializer.setPluginCallback(this::registerPlugin);
-        RequiemGamerules.init();
     }
 
     private void registerSubDataManagers() {
