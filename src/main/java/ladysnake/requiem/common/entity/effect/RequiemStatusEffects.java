35ae971b835fdd8f37028ea6d511a19fc6c22502
Pyrofab <redstoneinfire@gmail.com>
2020-01-10 01:34:12 +0100

    Implement Attrition's effect and visuals

--- src/main/java/ladysnake/requiem/common/entity/effect/RequiemStatusEffects.java
+++ src/main/java/ladysnake/requiem/common/entity/effect/RequiemStatusEffects.java
@@ -1,19 +1,18 @@
 package ladysnake.requiem.common.entity.effect;
 
 import ladysnake.requiem.Requiem;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
 import net.minecraft.util.registry.Registry;
 
 public final class RequiemStatusEffects {
-    public static final StatusEffect ATTRITION = createEffect(StatusEffectType.HARMFUL, 0xAA3322);
+    public static final StatusEffect ATTRITION = new AttritionStatusEffect(StatusEffectType.HARMFUL, 0xAA3322)
+        .addAttributeModifier(EntityAttributes.MAX_HEALTH, "069ae0b1-4014-41dd-932f-a5da4417d711", -0.2, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);
 
     public static void init() {
         registerEffect(ATTRITION, "attrition");
-    }
-
-    public static StatusEffect createEffect(StatusEffectType type, int color) {
-        return new StatusEffect(type, color) {};
     }
 
     public static void registerEffect(StatusEffect effect, String name) {
