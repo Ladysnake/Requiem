b65338f45b5621cd83d6faf8c5707115c2197552
Pyrofab <redstoneinfire@gmail.com>
2020-01-11 19:44:54 +0100

    Hijack enchanted book rendering

--- src/main/java/ladysnake/requiem/common/enchantment/RequiemEnchantments.java
+++ src/main/java/ladysnake/requiem/common/enchantment/RequiemEnchantments.java
@@ -4,18 +4,16 @@ import ladysnake.requiem.Requiem;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class RequiemEnchantments {
+    public static final Identifier HUMANITY_ID = Requiem.id("humanity");
     public static final Enchantment HUMANITY = new HumanityEnchantment(
         Enchantment.Weight.RARE, EnchantmentTarget.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND}
     );
 
     public static void init() {
-        registerEnchantment(HUMANITY, "humanity");
-    }
-
-    public static void registerEnchantment(Enchantment enchant, String name) {
-        Registry.register(Registry.ENCHANTMENT, Requiem.id(name), enchant);
+        Registry.register(Registry.ENCHANTMENT, HUMANITY_ID, HUMANITY);
     }
 }
