896d0d4fb8882ce6d56432eb56211c9f087db0b9
Pyrofab <redstoneinfire@gmail.com>
2020-02-21 12:27:39 +0100

    Make the horologist mirror damage

--- src/main/java/ladysnake/requiem/common/entity/HorologistEntity.java
+++ src/main/java/ladysnake/requiem/common/entity/HorologistEntity.java
@@ -2,12 +2,16 @@ package ladysnake.requiem.common.entity;
 
 import ladysnake.requiem.Requiem;
 import ladysnake.requiem.api.v1.RequiemPlayer;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.Npc;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -15,6 +19,26 @@ import javax.annotation.Nullable;
 public class HorologistEntity extends PassiveEntity implements Npc {
     public HorologistEntity(EntityType<? extends HorologistEntity> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    public net.minecraft.entity.EntityData initialize(IWorld world, LocalDifficulty difficulty, SpawnType spawnType, @Nullable net.minecraft.entity.EntityData entityData, @Nullable CompoundTag entityTag) {
+        if (entityData == null) {
+            entityData = new PassiveEntity.EntityData();
+            ((PassiveEntity.EntityData)entityData).setBabyAllowed(false);
+        }
+
+        return super.initialize(world, difficulty, spawnType, entityData, entityTag);
+    }
+
+    @Override
+    protected float getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions) {
+        return this.isBaby() ? 0.81F : 1.62F;
+    }
+
+    @Override
+    public boolean canImmediatelyDespawn(double distanceSquared) {
+        return false;
     }
 
     @Nullable
@@ -27,6 +51,25 @@ public class HorologistEntity extends PassiveEntity implements Npc {
     protected void initGoals() {
         super.initGoals();
         this.goalSelector.add(10, new LookAtEntityGoal(this, PlayerEntity.class, 32.0F));
+    }
+
+    @Override
+    public boolean damage(DamageSource source, float amount) {
+        if (source.getAttacker() instanceof LivingEntity) {
+            LivingEntity attacker = (LivingEntity) source.getAttacker();
+            this.swapPosition(attacker);
+            attacker.damage(source, amount);
+            return false;
+        }
+        return super.damage(source, amount);
+    }
+
+    private void swapPosition(LivingEntity attacker) {
+        Vec3d pos = attacker.getPos();
+        float yaw = attacker.yaw;
+        float pitch = attacker.pitch;
+        attacker.copyPositionAndRotation(this);
+        this.refreshPositionAndAngles(pos.x, pos.y, pos.z, yaw, pitch);
     }
 
     @Override
