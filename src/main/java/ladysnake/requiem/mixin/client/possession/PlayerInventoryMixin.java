907d1b313916b809b2ddec46f641d59d056c4f8a
Pyrofab <redstoneinfire@gmail.com>
2020-11-20 17:07:31 +0100

    Prevent adding stacks to locked slots

--- src/main/java/ladysnake/requiem/mixin/client/possession/PlayerInventoryMixin.java
+++ src/main/java/ladysnake/requiem/mixin/client/possession/PlayerInventoryMixin.java
@@ -4,6 +4,7 @@ import ladysnake.requiem.api.v1.entity.InventoryLimiter;
 import ladysnake.requiem.common.impl.inventory.PlayerInventoryLimiter;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerInventory.class)
 public abstract class PlayerInventoryMixin {
@@ -27,6 +29,13 @@ public abstract class PlayerInventoryMixin {
     private void preventHotbarSelection(CallbackInfo ci) {
         if (InventoryLimiter.KEY.get(this.player).isSlotLocked(this.selectedSlot)) {
             this.selectedSlot = PlayerInventoryLimiter.MAINHAND_SLOT;
+        }
+    }
+
+    @Inject(method = "addStack(ILnet/minecraft/item/ItemStack;)I", at = @At("HEAD"))
+    private void preventAddStack(int slot, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        if (InventoryLimiter.KEY.get(this.player).isSlotLocked(slot)) {
+            cir.setReturnValue(stack.getCount());
         }
     }
 }
