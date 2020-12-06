20dd5a3172113bb88b5aa4c09d058fc6146e0372
Pyrofab <redstoneinfire@gmail.com>
2020-12-06 15:19:46 +0100

    Actually fix the crash upon death

--- src/main/java/ladysnake/requiem/mixin/common/remnant/EntityPredicatesMixin.java
+++ src/main/java/ladysnake/requiem/mixin/common/remnant/EntityPredicatesMixin.java
@@ -34,7 +34,6 @@
  */
 package ladysnake.requiem.mixin.common.remnant;
 
-import dev.onyxstudios.cca.api.v3.component.ComponentProvider;
 import ladysnake.requiem.api.v1.remnant.RemnantComponent;
 import net.minecraft.entity.Entity;
 import net.minecraft.predicate.entity.EntityPredicates;
@@ -44,13 +43,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
+
 @Mixin(EntityPredicates.class)
 public abstract class EntityPredicatesMixin {
     @Dynamic("Lambda method injection")
     @Inject(method = {"method_5910", "method_24517"}, at = @At("RETURN"), cancellable = true)
-    private static void exceptCreativeOrSpectator(Entity tested, CallbackInfoReturnable<Boolean> info) {
-        // Can apparently be called in the constructor, so need to check that components have been initialized
-        if (info.getReturnValueZ() && ComponentProvider.fromEntity(tested).getComponentContainer() != null && RemnantComponent.isSoul(tested)) {
+    private static void exceptCreativeOrSpectator(@Nullable Entity tested, CallbackInfoReturnable<Boolean> info) {
+        if (info.getReturnValueZ() && tested != null && RemnantComponent.isSoul(tested)) {
             info.setReturnValue(false);
         }
     }
