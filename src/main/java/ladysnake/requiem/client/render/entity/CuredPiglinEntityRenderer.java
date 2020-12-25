880d69fe7666f18787f1e0864a35cb86d219e383
Pyrofab <redstoneinfire@gmail.com>
2020-12-25 23:13:57 +0100

    Fix cured piglins crashing the client

--- src/main/java/ladysnake/requiem/client/render/entity/CuredPiglinEntityRenderer.java
+++ src/main/java/ladysnake/requiem/client/render/entity/CuredPiglinEntityRenderer.java
@@ -2,6 +2,7 @@ package ladysnake.requiem.client.render.entity;
 
 import com.google.common.collect.ImmutableMap;
 import ladysnake.requiem.Requiem;
+import ladysnake.requiem.common.entity.RequiemEntities;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.PiglinEntityRenderer;
 import net.minecraft.entity.EntityType;
@@ -12,8 +13,8 @@ import java.util.Map;
 
 public class CuredPiglinEntityRenderer extends PiglinEntityRenderer {
     private static final Map<EntityType<?>, Identifier> piglinTextures = ImmutableMap.of(
-        EntityType.PIGLIN, Requiem.id("textures/entity/cured_piglin.png"),
-        EntityType.PIGLIN_BRUTE, Requiem.id("textures/entity/cured_piglin_brute.png")
+        RequiemEntities.CURED_PIGLIN, Requiem.id("textures/entity/cured_piglin.png"),
+        RequiemEntities.CURED_PIGLIN_BRUTE, Requiem.id("textures/entity/cured_piglin_brute.png")
     );
 
     @Override
