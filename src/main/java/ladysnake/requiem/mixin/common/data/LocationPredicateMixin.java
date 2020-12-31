35be3750e7cb61f4245d282b8a665c6a3b2a692c
Pyrofab <redstoneinfire@gmail.com>
2020-12-31 17:53:20 +0100

    Add drowned resurrection

---
+++ src/main/java/ladysnake/requiem/mixin/common/data/LocationPredicateMixin.java
@@ -0,0 +1,78 @@
+/*
+ * Requiem
+ * Copyright (C) 2017-2020 Ladysnake
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses>.
+ *
+ * Linking this mod statically or dynamically with other
+ * modules is making a combined work based on this mod.
+ * Thus, the terms and conditions of the GNU General Public License cover the whole combination.
+ *
+ * In addition, as a special exception, the copyright holders of
+ * this mod give you permission to combine this mod
+ * with free software programs or libraries that are released under the GNU LGPL
+ * and with code included in the standard release of Minecraft under All Rights Reserved (or
+ * modified versions of such code, with unchanged license).
+ * You may copy and distribute such a system following the terms of the GNU GPL for this mod
+ * and the licenses of the other code concerned.
+ *
+ * Note that people who make modified versions of this mod are not obligated to grant
+ * this special exception for their modified versions; it is their choice whether to do so.
+ * The GNU General Public License gives permission to release a modified version without this exception;
+ * this exception also makes it possible to release a modified version which carries forward this exception.
+ */
+package ladysnake.requiem.mixin.common.data;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.JsonOps;
+import ladysnake.requiem.Requiem;
+import net.minecraft.predicate.entity.LocationPredicate;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(LocationPredicate.class)
+public abstract class LocationPredicateMixin {
+    private @Nullable Biome.Category requiem$biomeCategory;
+
+    @Inject(method = "test(Lnet/minecraft/server/world/ServerWorld;FFF)Z", at = @At("RETURN"), cancellable = true)
+    private void test(ServerWorld world, float x, float y, float z, CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValueZ() && this.requiem$biomeCategory != null) {
+            BlockPos blockPos = new BlockPos(x, y, z);
+            Biome biome = world.getBiome(blockPos);
+
+            if (this.requiem$biomeCategory != biome.getCategory()) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+
+    // ANY return is actually an early return in the bytecode
+    @Inject(method = "fromJson", at = @At(value = "RETURN", ordinal = 1), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private static void fromJson(JsonElement json, CallbackInfoReturnable<LocationPredicate> cir, JsonObject locationData) {
+        if (locationData.has("requiem$biome_category")) {
+            //noinspection ConstantConditions
+            ((LocationPredicateMixin) (Object) cir.getReturnValue()).requiem$biomeCategory
+                = Biome.Category.CODEC.parse(JsonOps.INSTANCE, locationData.get("requiem$biome_category"))
+                .getOrThrow(false, msg -> Requiem.LOGGER.error("[Requiem] Failed to parse biome_category extension to LocationPredicate: {}", msg));
+        }
+    }
+}
---
+++ src/main/java/ladysnake/requiem/mixin/common/data/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Data extensions for minecraft's resource loaders
+ */
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package ladysnake.requiem.mixin.common.data;
+
+import ladysnake.requiem.api.v1.annotation.FieldsAreNonnullByDefault;
+import ladysnake.requiem.api.v1.annotation.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
