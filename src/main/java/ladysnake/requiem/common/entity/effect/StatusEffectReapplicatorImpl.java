0469d3260c85de028c0c011b01ad0056b0a49ccf
stormdirus2 <78043489+stormdirus2@users.noreply.github.com>
2021-01-31 11:28:43 -0600

    Make complacent with changes before.

--- src/main/java/ladysnake/requiem/common/entity/effect/StatusEffectReapplicatorImpl.java
+++ src/main/java/ladysnake/requiem/common/entity/effect/StatusEffectReapplicatorImpl.java
@@ -59,7 +59,7 @@ public class StatusEffectReapplicatorImpl implements StatusEffectReapplicator {
         if (!this.holder.world.isClient) {
             if (effect.getEffectType() == RequiemStatusEffects.ATTRITION && effect.getAmplifier() > 0) {
                 if (effect.getDuration() == 0) {
-                    if (StickyStatusEffect.shouldStick(effect.getEffectType(), this.holder)) {
+                    if (StickyStatusEffect.shouldNotFade(effect.getEffectType(), this.holder)) {
                         AttritionStatusEffect.addAttrition(this.holder,effect.getAmplifier() + 1);
                     } else {
                         AttritionStatusEffect.addAttrition(this.holder,effect.getAmplifier() - 1);
