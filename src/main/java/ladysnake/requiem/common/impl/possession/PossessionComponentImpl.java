709338258c8450469a7fc62f62896b58e272aea8
stormdirus2 <78043489+stormdirus2@users.noreply.github.com>
2021-01-30 21:10:31 -0600

    Update PossessionComponentImpl.java
    Test.

--- src/main/java/ladysnake/requiem/common/impl/possession/PossessionComponentImpl.java
+++ src/main/java/ladysnake/requiem/common/impl/possession/PossessionComponentImpl.java
@@ -80,7 +80,8 @@ public final class PossessionComponentImpl implements PossessionComponent {
     }
 
     private boolean isReadyForPossession() {
-        return player.world.isClient || (!player.isSpectator() && RemnantComponent.get(this.player).isIncorporeal());
+        StatusEffectInstance attrition = player.getStatusEffect(RequiemStatusEffects.ATTRITION)
+        return player.world.isClient || (!player.isSpectator() && RemnantComponent.get(this.player).isIncorporeal()) && !(attrition != null && attrition.getAmplifier() < 4);
     }
 
     /**
