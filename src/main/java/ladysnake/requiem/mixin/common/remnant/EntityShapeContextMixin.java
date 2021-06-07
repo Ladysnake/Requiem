6371771282ac897f3ec548aa5544f94a881b3b52
Pyrofab <redstoneinfire@gmail.com>
2021-06-07 20:28:26 +0200

    Use prefix for conflicting mixin field, fixes #292

--- src/main/java/ladysnake/requiem/mixin/common/remnant/EntityShapeContextMixin.java
+++ src/main/java/ladysnake/requiem/mixin/common/remnant/EntityShapeContextMixin.java
@@ -40,23 +40,21 @@ import net.minecraft.block.EntityShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityShapeContext.class)
 public abstract class EntityShapeContextMixin implements ExtendedShapeContext {
-    @Unique
-    private boolean noClipping;
+    private boolean requiem$noClipping;
 
     @Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/entity/Entity;)V")
     private void setEntityField(Entity entity, CallbackInfo info) {
-        this.noClipping = entity instanceof PlayerEntity && MovementAlterer.KEY.get(entity).isNoClipping();
+        this.requiem$noClipping = entity instanceof PlayerEntity && MovementAlterer.KEY.get(entity).isNoClipping();
     }
 
     @Override
     public boolean requiem_isNoClipping() {
-        return this.noClipping;
+        return this.requiem$noClipping;
     }
 }
