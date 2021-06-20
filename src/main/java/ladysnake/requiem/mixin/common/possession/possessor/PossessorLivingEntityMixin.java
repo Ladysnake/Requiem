8da3882e2778393b2d39e1dee5aa921f5f1f316e
Pyrofab <redstoneinfire@gmail.com>
2021-06-20 09:34:58 +0200

    Fix fall damage never being taken during possession

--- src/main/java/ladysnake/requiem/mixin/common/possession/possessor/PossessorLivingEntityMixin.java
+++ src/main/java/ladysnake/requiem/mixin/common/possession/possessor/PossessorLivingEntityMixin.java
@@ -56,7 +56,11 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Invoker;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -130,7 +134,7 @@ public abstract class PossessorLivingEntityMixin extends PossessorEntityMixin {
         if (this.requiem$getWorld().isClient) return;
 
         Entity possessed = PossessionComponent.getPossessedEntity((Entity) (Object) this);
-        if (possessed != null && fallY > 0) {
+        if (possessed != null && this.requiem$getFallDistance() > 0) {
             possessed.fallDistance = this.requiem$getFallDistance();
             possessed.copyPositionAndRotation((Entity) (Object) this);
             possessed.move(MovementType.SELF, Vec3d.ZERO);
